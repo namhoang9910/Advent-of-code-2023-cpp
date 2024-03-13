ffbeea0d96f5b0dd74709b8a9b23c7f312601ee6
namhoang9910 <namhoang9910@gmail.com>
2024-03-14 10:42:15 +1100

    Version 1 - add textToDigit function

--- 1b.cpp
+++ 1b.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+string textToDigit(string lineString);
+
 int main() 
 {
     ifstream inputFile;
@@ -19,50 +21,8 @@ int main()
         {
             if (!lineString.empty())
             {
-                vector<string> textNumbers = {"zero", "one", "two", "three", "four", 
-                                              "five", "six", "seven", "eight", "nine"};
-
-                size_t firstTextNumIdx = string::npos;
-                size_t lastTextNumIdx = string::npos;
-                int firstNumLen = 0, lastNumLen = 0;
-                int firstNumCount = 0, lastNumCount = 0, currentCount = 0;
-                for (const string& textNumber : textNumbers)
-                {
-                    size_t currentFirstIdx = lineString.find(textNumber);
-                    if (currentFirstIdx != string::npos && (firstTextNumIdx == string::npos || currentFirstIdx < firstTextNumIdx))
-                    {
-                        firstTextNumIdx = currentFirstIdx;
-                        firstNumLen = textNumber.length();
-                        firstNumCount = currentCount;
-                    }
-
-                    size_t currentLastIdx = lineString.rfind(textNumber);
-                    if (currentLastIdx != string::npos && (lastTextNumIdx == string::npos || currentLastIdx > lastTextNumIdx))
-                    {
-                        lastTextNumIdx = currentLastIdx;
-                        lastNumLen = textNumber.length();
-                        lastNumCount = currentCount;
-                    }
-                    
-                    currentCount++; 
-                }
-
-                if (lastNumLen > 0)
-                {
-                    if ((firstTextNumIdx + firstNumLen - 1) == lastTextNumIdx)
-                    {
-                        lineString.replace(lastTextNumIdx + 1, lastNumLen - 1, to_string(lastNumCount));
-                    }
-                    else
-                    {
-                        lineString.replace(lastTextNumIdx, lastNumLen, to_string(lastNumCount));
-                    }
-                }
+                lineString = textToDigit(lineString);
                 
-                if (firstNumLen > 0)
-                {
-                    lineString.replace(firstTextNumIdx, firstNumLen, to_string(firstNumCount));
-                }
 
                 size_t firstDigitIdx = lineString.find_first_of("0123456789");
                 size_t lastDigitIdx = lineString.find_last_of("0123456789");
@@ -88,4 +48,54 @@ int main()
     cout << "----------------------------------------------\n";
 
     return 0;
+}
+
+string textToDigit(string lineString)
+{
+    vector<string> textNumbers = {"zero", "one", "two", "three", "four",
+                                  "five", "six", "seven", "eight", "nine"};
+
+    size_t firstTextNumIdx = string::npos;
+    size_t lastTextNumIdx = string::npos;
+    int firstNumLen = 0, lastNumLen = 0;
+    int firstNumCount = 0, lastNumCount = 0, currentCount = 0;
+    for (const string &textNumber : textNumbers)
+    {
+        size_t currentFirstIdx = lineString.find(textNumber);
+        if (currentFirstIdx != string::npos && (firstTextNumIdx == string::npos || currentFirstIdx < firstTextNumIdx))
+        {
+            firstTextNumIdx = currentFirstIdx;
+            firstNumLen = textNumber.length();
+            firstNumCount = currentCount;
+        }
+
+        size_t currentLastIdx = lineString.rfind(textNumber);
+        if (currentLastIdx != string::npos && (lastTextNumIdx == string::npos || currentLastIdx > lastTextNumIdx))
+        {
+            lastTextNumIdx = currentLastIdx;
+            lastNumLen = textNumber.length();
+            lastNumCount = currentCount;
+        }
+
+        currentCount++;
+    }
+
+    if (lastNumLen > 0)
+    {
+        if ((firstTextNumIdx + firstNumLen - 1) == lastTextNumIdx)
+        {
+            lineString.replace(lastTextNumIdx + 1, lastNumLen - 1, to_string(lastNumCount));
+        }
+        else
+        {
+            lineString.replace(lastTextNumIdx, lastNumLen, to_string(lastNumCount));
+        }
+    }
+
+    if (firstNumLen > 0)
+    {
+        lineString.replace(firstTextNumIdx, firstNumLen, to_string(firstNumCount));
+    }
+
+    return lineString;
 }
